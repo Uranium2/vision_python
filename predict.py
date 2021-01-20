4be037227e5a5915b6afe44e369bdaca78a6b444
Tavernier Antoine <tavernier.antoine@hotmail.fr>
2021-01-20 16:15:20 +0100

    adapt to one file

--- predict.py
+++ predict.py
@@ -12,10 +12,6 @@ PATH_TO_TEST_IMAGES_DIR =  "test"
 
 assert os.path.isfile(PATH_TO_CKPT)
 assert os.path.isfile(PATH_TO_LABELS)
-TEST_IMAGE_PATHS = glob.glob(os.path.join(PATH_TO_TEST_IMAGES_DIR, "*.*"))
-assert len(TEST_IMAGE_PATHS) > 0, 'No image found in `{}`.'.format(PATH_TO_TEST_IMAGES_DIR)
-print(TEST_IMAGE_PATHS)
-
 
 import numpy as np
 import os
@@ -122,25 +118,43 @@ def run_inference_for_single_image(image, graph):
     return output_dict
 
 
-for image_path in TEST_IMAGE_PATHS:
-    image = Image.open(image_path)
-    print(image_path)
-    # the array based representation of the image will be used later in order to prepare the
-    # result image with boxes and labels on it.
-    image_np = load_image_into_numpy_array(image)
-    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-    image_np_expanded = np.expand_dims(image_np, axis=0)
-    # Actual detection.
-    output_dict = run_inference_for_single_image(image_np, detection_graph)
-    # Visualization of the results of a detection.
-    vis_util.visualize_boxes_and_labels_on_image_array(
-        image_np,
-        output_dict['detection_boxes'],
-        output_dict['detection_classes'],
-        output_dict['detection_scores'],
-        category_index,
-        instance_masks=output_dict.get('detection_masks'),
-        use_normalized_coordinates=True,
-        line_thickness=8)
-    plt.figure(figsize=IMAGE_SIZE)
-    plt.imshow(image_np)
+
+image_path = "img/to_predict.jpg"
+image = Image.open(image_path)
+print(image_path)
+# the array based representation of the image will be used later in order to prepare the
+# result image with boxes and labels on it.
+image_np = load_image_into_numpy_array(image)
+# Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+image_np_expanded = np.expand_dims(image_np, axis=0)
+# Actual detection.
+output_dict = run_inference_for_single_image(image_np, detection_graph)
+
+index_best_prediction = np.argmax(output_dict['detection_scores'])
+
+score_best_prediction = output_dict['detection_scores'][index_best_prediction]
+box_best_prediction = output_dict['detection_boxes'][index_best_prediction]
+classe_best_prediction = output_dict['detection_classes'][index_best_prediction]
+
+print(box_best_prediction)
+
+vis_util.visualize_boxes_and_labels_on_image_array(
+	image_np,
+	output_dict['detection_boxes'],
+	output_dict['detection_classes'],
+	output_dict['detection_scores'],
+	category_index,
+	instance_masks=output_dict.get('detection_masks'),
+	use_normalized_coordinates=True,
+	line_thickness=8)
+
+import matplotlib
+matplotlib.use('TkAgg')
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+imgplot = plt.imshow(image_np)
+plt.show()
+
+
